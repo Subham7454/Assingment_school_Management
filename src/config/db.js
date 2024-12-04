2887634dd36665537b19d04122b0ac3c5a889e99
Subham <shubhamrawat1843@gmail.com>
2024-12-04 17:00:54 +0530

    changes in env variables and db code

--- src/config/db.js
+++ src/config/db.js
@@ -6,10 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    uri: process.env.DB_URI,
 });
 
 
